4da151f4b898bb735217bd70b99bc1a877966e85
kiejaana <100525896+kyanamarckx@users.noreply.github.com>
2022-10-27 16:46:37 +0200

    edit mock-data

---
+++ src/data/mock-data.js
@@ -0,0 +1,40 @@
+let BESTEMMINGEN = [
+  {
+    id: 1,
+    land: 'België',
+    stad: 'Gent',
+    postcode: 9000,
+  },
+  {
+    id: 2,
+    land: 'Frankrijk',
+    stad: 'Cotignac',
+    postcode: 83570,
+  },
+  {
+    id: 3,
+    land: 'Spanje',
+    stad: 'Valencia',
+    postcode: 46001,
+  },
+  {
+    id: 4,
+    land: 'Griekenland',
+    stad: 'Santorini',
+    postcode: 84700,
+  },
+  {
+    id: 5,
+    land: 'Frankrijk',
+    stad: 'Bora Bora',
+    postcode: 98730,
+  },
+];
+
+let VERVOERSMIDDELEN = [
+
+];
+
+let REIZIGERS = [
+
+];
