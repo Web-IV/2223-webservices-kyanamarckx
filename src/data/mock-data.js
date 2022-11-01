5860dd401772b6dc70dbc4eb12820d9fa4ad59f9
kiejaana <100525896+kyanamarckx@users.noreply.github.com>
2022-11-01 22:09:27 +0100

    edit mock-data

--- src/data/mock-data.js
+++ src/data/mock-data.js
@@ -32,9 +32,106 @@ let BESTEMMINGEN = [
 ];
 
 let VERVOERSMIDDELEN = [
-
+  {
+    id: 1,
+    type: 'auto',
+  },
+  {
+    id: 2,
+    type: 'trein',
+  },
+  {
+    id: 3,
+    type: 'vliegtuig',
+  },
+  {
+    id: 4,
+    type: 'boot',
+  },
+  {
+    id: 5,
+    type: 'bus',
+  },
 ];
 
 let REIZIGERS = [
-
+  {
+    id: 1,
+    voornaam: 'Kyana',
+    naam: 'Marckx',
+    geboortedatum: '2003-10-01',
+    stad: 'Lierde',
+    straat: 'Keistraat',
+    huisnummer: '1C',
+  },
+  {
+    id: 2,
+    voornaam: 'Robin',
+    naam: 'De Waegeneer',
+    geboortedatum: '2003-12-15',
+    stad: 'Aalst',
+    straat: 'Hugo Lefèvrestraat',
+    huisnummer: '5',
+  },
+  {
+    id: 3,
+    voornaam: 'Greta',
+    naam: 'Van der Linden',
+    geboortedatum: '1959-12-09',
+    stad: 'Lierde',
+    straat: 'Molenstraat',
+    huisnummer: '10',
+  },
+  {
+    id: 4,
+    voornaam: 'Tine',
+    naam: 'Van Voren',
+    geboortedatum: '2004-10-25',
+    stad: 'Bever',
+    straat: 'Akrenbos',
+    huisnummer: '51A',
+  },
+  {
+    id: 5,
+    voornaam: 'Lorenz',
+    naam: 'Coppens',
+    geboortedatum: '1992-07-30',
+    stad: 'Gent',
+    straat: 'Keramiekstraat',
+    huisnummer: '80',
+  },
 ];
+
+let VERPLAATSINGEN = [
+  {
+    id: 1,
+    vervoersmiddelId: 3,
+    reizigerId: 2,
+    bestemmingId: 4,
+  },
+  {
+    id: 2,
+    vervoersmiddelId: 1,
+    reizigerId: 3,
+    bestemmingId: 2,
+  },
+  {
+    id: 3,
+    vervoersmiddelId: 2,
+    reizigerId: 4,
+    bestemmingId: 1,
+  },
+  {
+    id: 4,
+    vervoersmiddelId: 5,
+    reizigerId: 1,
+    bestemmingId: 3,
+  },
+  {
+    id: 5,
+    vervoersmiddelId: 4,
+    reizigerId: 5,
+    bestemmingId: 5,
+  },
+];
+
