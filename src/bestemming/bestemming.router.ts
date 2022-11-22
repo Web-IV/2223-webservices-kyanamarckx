0e019a915b61afadf3dd1491eaa0514712a13d5e
kiejaana <100525896+kyanamarckx@users.noreply.github.com>
2022-11-22 19:43:50 +0100

    CRUD operations bestemming

---
+++ src/bestemming/bestemming.router.ts
@@ -0,0 +1,85 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+
+import * as BestemmingService from './bestemming.service';
+
+export const bestemmingRouter = express.Router();
+
+// GET: list of all Bestemmingen
+bestemmingRouter.get('/', async (req: Request, res: Response) => {
+  try {
+    const bestemmingen = await BestemmingService.listBestemmingen();
+    return res.status(200).json(bestemmingen);
+  } catch (error: any) {
+    return res.status(500).json(error.message);
+  }
+});
+
+// GET: Bestemming by id
+bestemmingRouter.get('/:id', async (req: Request, res: Response) => {
+  const id: number = parseInt(req.params.id, 10);
+  try {
+    const bestemming = await BestemmingService.getBestemmingById(id);
+    if (bestemming) {
+      return res.status(200).json(bestemming);
+    }
+    return res.status(404).json('Bestemming niet gevonden');
+  } catch (error: any) {
+    return res.status(500).json(error.message);
+  }
+});
+
+//POST: create a new Bestemming
+// params: land, stad, postcode
+bestemmingRouter.post("/",
+body("land").isString(),
+body("stad").isString(),
+body("postcode").isString(),
+async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const id = parseInt(req.params.id, 10);
+  try {
+    const bestemming = req.body;
+    const newBestemming = await BestemmingService.createBestemming(bestemming, id);
+    return res.status(201).json(newBestemming);
+  } catch (error: any) {
+    return res.status(500).json(error.message);
+  }
+});
+
+// PUT: update a Bestemming
+// params: land, stad, postcode
+bestemmingRouter.put("/:id",
+body("land").isString(),
+body("stad").isString(),
+body("postcode").isString(),
+async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const id: number = parseInt(req.params.id, 10);
+  try {
+    const bestemming = req.body;
+    const updatedBestemming = await BestemmingService.updateBestemming(id, bestemming);
+    return res.status(200).json(updatedBestemming);
+  } catch (error: any) {
+    return res.status(500).json(error.message);
+  }
+});
+
+// DELETE: delete a Bestemming
+bestemmingRouter.delete("/:id", async (req: Request, res: Response) => {
+  const id: number = parseInt(req.params.id, 10);
+  try {
+    await BestemmingService.deleteBestemming(id);
+    return res.status(204).json("Bestemming werd succesvol verwijderd");
+  } catch (error: any) {
+    return res.status(500).json(error.message);
+  }
+});
+
---
+++ src/bestemming/bestemming.service.ts
@@ -0,0 +1,81 @@
+import { db } from "../utils/db.server";
+
+type Bestemming = {
+  id: number;
+  land: string;
+  stad: string;
+  postcode: string;
+}
+
+export const listBestemmingen = async (): Promise<Bestemming[]> => {
+  return db.bestemming.findMany({
+    select: {
+      id: true,
+      land: true,
+      stad: true,
+      postcode: true,
+    },
+  });
+};
+
+export const getBestemmingById = async(id: number): Promise<Bestemming | null> => {
+  return db.bestemming.findUnique({
+    where: {
+      id,
+    },
+    // select: {
+    //   id: true,
+    //   land: true,
+    //   stad: true,
+    //   postcode: true,
+    // },
+  });
+};
+
+export const createBestemming = async(bestemming: Omit<Bestemming, "id">, id: number):
+Promise<Bestemming> => {
+  const { land, stad, postcode } = bestemming
+  return db.bestemming.create({
+    data: {
+      land,
+      stad,
+      postcode,
+    },
+    select: {
+      id: true,
+      land: true,
+      stad: true,
+      postcode: true,
+    },
+  });
+};
+
+export const updateBestemming = async(id: number, bestemming: Bestemming):
+Promise<Bestemming | null> => {
+  const { land, stad, postcode } = bestemming
+  return db.bestemming.update({
+    where: {
+      id,
+    },
+    data: {
+      land,
+      stad,
+      postcode,
+    },
+    select: {
+      id: true,
+      land: true,
+      stad: true,
+      postcode: true,
+    },
+  });
+};
+
+export const deleteBestemming = async(id: number): 
+Promise<void> => {
+  await db.bestemming.delete({
+    where: {
+      id,
+    },
+  });
+};
