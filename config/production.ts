bc7e2d7aa3050380e5cc19d1c5520d919ee35fb1
kiejaana <100525896+kyanamarckx@users.noreply.github.com>
2022-12-20 22:38:56 +0100

    edit production

--- config/production.ts
+++ config/production.ts
@@ -1,11 +1,11 @@
 module.exports = {
-  port: 8000,
+  port: 40043,
   log: {
     level: 'info',
     disabled: false,
   },
   cors: {
-    origins: ['http://localhost:8000'],
+    origins: ['http://localhost:40043'],
     maxAge: 3 * 60 * 60,
   }
 };
